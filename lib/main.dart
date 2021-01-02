e19c747e2629ac98f00fb612f901002aec156f6f
yusukemisa <yusukemisa@mercari.com>
2021-01-03 00:50:54 +0900

    Impl 桐生ファイナンス

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -6,10 +9,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '桐生ファイナンス',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,9 +24,10 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      navigatorObservers: [],
+      home: MyHomePage(title: '桐生ファイナンス'),
     );
   }
 }
@@ -59,50 +64,26 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  // void _openDrawer() {
+  //    _scaffoldKey.currentState.openDrawer();
+  // }
+
+  void _closeDrawer() {
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    //getData();
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      drawer: drawer(context),
+      // Disable opening the drawer with a swipe gesture.
+      drawerEnableOpenDragGesture: true,
+      body: ColumnBody(context),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
@@ -110,4 +91,183 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  // Widget Body1(BuildContext context) {
+  //   return Center(
+  //     child: Column(
+  //       mainAxisAlignment: MainAxisAlignment.center,
+  //       children: <Widget>[
+  //         Text(
+  //           'You have pushed the button this many times:',
+  //         ),
+  //         Text(
+  //           '$_counter',
+  //           style: Theme.of(context).textTheme.headline4,
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+
+  // Widget CardBody(BuildContext context) {
+  //   return Center(
+  //     child: SizedBox(
+  //         height: 100.0,
+  //         width: 300,
+  //         child: Card(
+  //           child: Text('card!'),
+  //         )),
+  //   );
+  // }
+
+  Map data;
+  List shinogiList;
+  var url = 'https://kiryu-finance-dot-kiryu-coco-dev.an.r.appspot.com/';
+
+  Future getData() async {
+    http.Response response = await http.get(url + 'shinogi');
+    data = json.decode(response.body); //json->Mapオブジェクトに格納
+    print('getData');
+    setState(() {
+      shinogiList = data["shinogiList"]; //Map->Listに必要な情報だけ格納
+    });
+  }
+
+  Widget ListBody(BuildContext context) {
+    return ListView.builder(
+      //スクロール可能な可変リストを作る
+      shrinkWrap: true,
+      itemCount: shinogiList == null ? 0 : shinogiList.length, //受け取る数の定義
+      itemBuilder: (BuildContext context, int index) {
+        //ここに表示したい内容をindexに応じて
+        print(shinogiList.length);
+        return Card(
+          child: ListTile(
+            leading: Image.network('https://placehold.jp/50x50.png'),
+            title: Text(shinogiList[index]["name"]),
+            subtitle: Text('報酬：￥' + shinogiList[index]["reward"].toString()),
+            trailing: Icon(Icons.more_vert),
+            onTap: () {},
+          ),
+        );
+      },
+    );
+  }
+
+  Widget ColumnBody(BuildContext context) {
+    return new Column(children: [
+      Container(
+        height: 300.0,
+        width: double.infinity,
+        // decoration: BoxDecoration(
+        //   border: Border.all(
+        //     color: Colors.red,
+        //     width: 8.0,
+        //   ),
+        // ),
+        child: Card(
+          child: Column(children: <Widget>[
+            const ListTile(
+              title: Text('残高'),
+            ),
+            ListTile(
+              title: Text('￥100,000,000'),
+              tileColor: Colors.cyan,
+            ),
+            const ListTile(
+              leading: Icon(Icons.account_balance),
+              title: Text('桐生会からお金を借りる'),
+              subtitle: Text('桐生会だから実際安心'),
+            ),
+            Row(
+              //mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                const SizedBox(height: 20),
+                FloatingActionButton.extended(
+                  icon: const Icon(Icons.add),
+                  label: Text('お金を借りる'),
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 20),
+                FloatingActionButton.extended(
+                  icon: const Icon(Icons.redo_sharp),
+                  label: Text('お金を返す'),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ]),
+        ),
+      ),
+      Container(
+        //height: 400.0,
+        // decoration: BoxDecoration(
+        //   border: Border.all(
+        //     color: Colors.blue,
+        //     width: 8.0,
+        //   ),
+        // ),
+        child: Column(children: [
+          const ListTile(
+            title: Text('シノギ'),
+          ),
+          ListBody(context),
+        ]),
+      ),
+    ]);
+  }
+//        child: Column(children: <Widget>[]),
+  // Widget ListBody(BuildContext context) {
+  //   List<Widget> list = <Widget>[
+  //     new ListTile(
+  //       title: new Text('Title1',
+  //           style: new TextStyle(color: Colors.green[600], fontSize: 16.0)),
+  //       leading: new Icon(
+  //         Icons.android,
+  //         color: Colors.green[600],
+  //       ),
+  //     ),
+  //     new ListTile(
+  //       title: new Text('Title2',
+  //           style: new TextStyle(color: Colors.green[600], fontSize: 16.0)),
+  //       leading: new Icon(
+  //         Icons.android,
+  //         color: Colors.green[600],
+  //       ),
+  //     ),
+  //   ];
+  //
+  //   return new ListView(
+  //     children: list,
+  //   );
+  // }
+
+  Widget drawer(BuildContext context) {
+    return Drawer(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('This is the Drawer'),
+            ElevatedButton(
+              onPressed: _closeDrawer,
+              child: const Text('Close Drawer'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SampleRequest {
+  final String key;
+  final int order;
+
+  SampleRequest({this.key, this.order});
+
+  Map<String, dynamic> toJson() => {
+        'key': key,
+        'order': order,
+      };
 }
